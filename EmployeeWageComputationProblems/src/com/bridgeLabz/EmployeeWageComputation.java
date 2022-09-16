c34127909623bca963388effbcf78ceac45bbc2f
Bharath-SR <bharath.s0941@gmail.com>
2022-09-16 15:30:35 +0530

    UC2: Calculated Daily Employee Wage

--- EmployeeWageComputationProblems/src/com/bridgeLabz/EmployeeWageComputation.java
+++ EmployeeWageComputationProblems/src/com/bridgeLabz/EmployeeWageComputation.java
@@ -12,23 +12,26 @@ import java.util.Random;
 public class EmployeeWageComputation {
 
 	/**
-	 * UC1: Check Employee is Present or Absent (Using Random Function)
+	 * UC2: Calculate Daily Employee Wage
 	 */
 	public static void main(String[] args) {
 		// Driving Class
 		
 		Random random = new Random();
-		int attendance = random.nextInt(2); 
+		int attendance = random.nextInt(3);
 
-		if (attendance == 1) {
-			System.out.println("Employee is Present");
-			
+		int wagePerHour = 20;
+		int fullDayHour = 8;
+		int wagePerDay = 0;
+
+		if (attendance == 2) {
+			System.out.println("Employee is Present Full Day");
+			wagePerDay = wagePerHour * fullDayHour;
 		}
-		 else {
+		 else
 			System.out.println("Employee is Absent");
 
-		}
-
+		System.out.println("Employee Wage Per Day is :" + wagePerDay);
 	}
 
 }
