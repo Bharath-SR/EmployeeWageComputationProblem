16182675637836604f010404b45a2338cc5805e0
Bharath-SR <bharath.s0941@gmail.com>
2022-09-16 16:29:45 +0530

    UC8:Employee Wage for Multiple Companies

--- EmployeeWageComputationProblems/src/com/bridgeLabz/EmployeeWageComputation.java
+++ EmployeeWageComputationProblems/src/com/bridgeLabz/EmployeeWageComputation.java
@@ -12,28 +12,33 @@ import java.util.Random;
 public class EmployeeWageComputation {
 
 	/**
-	 * UC6: Calculating Wages till a Condtion of total working hours or days is reached
+	 * UC8: Employee Wage for Multiple Companies
 	 */
-	 Random random = new Random();
-     int wagePerHour = 20;
-     int fullDayHour = 8;
-	 int partTimeHours = 4;
-	 int wagePerDay = 0;
-	 int monthlySalary = 0;
-	 int monthlyHour = 0;
-	 int days = 0;
+	static Random random = new Random();
 
-	public void EmployeeWage() {
+	static int wagePerHour = 20;
+	static int fullDayHour = 8;
+	static int partTimeHours = 4;
+	static int wagePerDay = 0;
+	static int monthlySalary = 0;
+	static int monthlyHour = 0;
+	static int days = 0;
+
+	static void EmployeeWage(String companyName, int empRatePerHour ,int  numOfWorkingDays , int maxHoursPerMonth) {
 		{
 			while (monthlyHour <= 100 || days <= 20) {
-                days++;
-                int attendance = random.nextInt(3); 
+
+				days++;
+
+				int attendance = random.nextInt(3); // Generate 3 random number 0,1,2
 				wagePerDay = 0;
 				switch (attendance) {
-                case 0:
+
+				case 0:
 					System.out.println("Employee Absent");
 					break;
-                case 1:
+
+				case 1:
 					System.out.println("Employee Part Time Present");
 					wagePerDay = partTimeHours * wagePerHour;
 					monthlyHour = monthlyHour + partTimeHours;
@@ -44,14 +49,20 @@ public class EmployeeWageComputation {
 					monthlyHour = monthlyHour + fullDayHour;
 					break;
 				}
+
 				monthlySalary = monthlySalary + wagePerDay;
 				System.out.println("Days: " + days + " :MonthlyHoursOfEmployee: " + monthlyHour + ": Monthly Salary Of Employee: " + monthlySalary);
 			}
 		}
+		 int totalEmpWage =monthlyHour * empRatePerHour;
+	        System.out.println("Total Employee wage for company: " + companyName+" is:" + totalEmpWage);
+	        return;
 	}
 
 	public static void main(String[] args) {
-		EmployeeWageComputation empwage =new EmployeeWageComputation();
-		empwage.EmployeeWage();
-}
+
+		EmployeeWage("Campany A",15,22,220);
+		EmployeeWage("Caompany B",20,20,200);
+
+	}
 }
