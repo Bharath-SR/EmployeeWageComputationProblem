f8322e20e59026afeeafa06de726d393ffe84e5c
Bharath-SR <bharath.s0941@gmail.com>
2022-09-16 15:20:22 +0530

    Started: Employee Wage Computation Problem

---
+++ EmployeeWageComputationProblems/src/com/bridgeLabz/EmployeeWageComputation.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.bridgeLabz;
+
+/**
+ * @author Admin
+ *
+ */
+public class EmployeeWageComputation {
+
+	/**
+	 * Welcome To Employee Wage Computation Program On Master Branch
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
